686593ac78164794dfe81c4b3fb7945c32fae89c
conlin-huang <995018884@qq.com>
2022-01-21 19:04:18 +0800

    update

--- aioscrapy/core/downloader/handlers/http.py
+++ aioscrapy/core/downloader/handlers/http.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class AioHttpDownloadHandler:
+    session = None
 
     def __init__(self, settings):
         self.settings = settings
@@ -22,6 +23,11 @@ class AioHttpDownloadHandler:
     @classmethod
     def from_settings(cls, settings):
         return cls(settings)
+
+    def get_session(self, *args, **kwargs):
+        if self.session is None:
+            self.session = aiohttp.ClientSession(*args, **kwargs)
+        return self.session
 
     async def download_request(self, request, spider):
         kwargs = {
@@ -36,18 +42,24 @@ class AioHttpDownloadHandler:
             headers = headers.to_unicode_dict()
         kwargs['headers'] = headers
 
-        proxy = request.meta.get("proxy")
-        if proxy:
-            kwargs["proxy"] = proxy
-            logger.info(f"使用代理{proxy}抓取: {request.url}")
-
         ssl_ciphers = request.meta.get('TLS_CIPHERS')
         if ssl_ciphers:
             context = ssl.create_default_context()
             context.set_ciphers(ssl_ciphers)
             kwargs['ssl'] = context
 
-        async with aiohttp.ClientSession(**self.aiohttp_client_session_args) as session:
+        proxy = request.meta.get("proxy")
+        if proxy:
+            kwargs["proxy"] = proxy
+            logger.info(f"使用代理{proxy}抓取: {request.url}")
+
+            async with aiohttp.ClientSession(**self.aiohttp_client_session_args) as session:
+                async with session.request(request.method, request.url, **kwargs) as response:
+                    content = await response.read()
+
+        # 当不用代理的时候，session不关闭
+        else:
+            session = self.get_session(**self.aiohttp_client_session_args)
             async with session.request(request.method, request.url, **kwargs) as response:
                 content = await response.read()
 
@@ -58,4 +70,9 @@ class AioHttpDownloadHandler:
                             cookies=response.cookies)
 
     async def close(self):
-        pass
+        if self.session is not None:
+            await self.session.close()
+
+        # Wait 250 ms for the underlying SSL connections to close
+        # https://docs.aiohttp.org/en/latest/client_advanced.html#graceful-shutdown
+        await asyncio.sleep(0.250)
