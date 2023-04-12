fa5c9634c1996413e3db2784ee79bbbc796a762a
unknown <huangconghua@sics.com>
2023-04-12 18:02:30 +0800

    feat: support requests

---
+++ aioscrapy/core/downloader/handlers/requests.py
@@ -0,0 +1,58 @@
+import asyncio
+import logging
+
+import requests
+
+from aioscrapy import Request
+from aioscrapy.core.downloader.handlers import BaseDownloadHandler
+from aioscrapy.http import HtmlResponse
+from aioscrapy.settings import Settings
+
+logger = logging.getLogger(__name__)
+
+
+class RequestsDownloadHandler(BaseDownloadHandler):
+
+    def __init__(self, settings):
+        self.settings: Settings = settings
+        self.verify_ssl: bool = self.settings.get("VERIFY_SSL")
+        self.loop = asyncio.get_running_loop()
+
+    @classmethod
+    def from_settings(cls, settings: Settings):
+        return cls(settings)
+
+    async def download_request(self, request: Request, _) -> HtmlResponse:
+        kwargs = {
+            'timeout': self.settings.get('DOWNLOAD_TIMEOUT'),
+            'cookies': dict(request.cookies),
+            'verify': self.verify_ssl,
+            'allow_redirects': self.settings.getbool('REDIRECT_ENABLED', True) if request.meta.get(
+                'dont_redirect') is None else request.meta.get('dont_redirect')
+        }
+        post_data = request.body or None
+        if isinstance(post_data, dict):
+            kwargs['json'] = post_data
+        else:
+            kwargs['data'] = post_data
+
+        headers = request.headers or self.settings.get('DEFAULT_REQUEST_HEADERS')
+        kwargs['headers'] = headers
+
+        proxy = request.meta.get("proxy")
+        if proxy:
+            kwargs["proxies"] = {'http': proxy, 'https': proxy}
+            logger.debug(f"use proxy {proxy}: {request.url}")
+
+        response = await asyncio.to_thread(requests.request, request.method, request.url, **kwargs)
+        return HtmlResponse(
+            response.url,
+            status=response.status_code,
+            headers=response.headers,
+            body=response.content,
+            cookies=dict(response.cookies),
+            encoding=response.encoding
+        )
+
+    async def close(self):
+        pass
